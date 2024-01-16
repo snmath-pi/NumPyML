dd17f0bae4eed367cb189f7d455a9e7f521bca33
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-01-16 10:03:17 +0530

    dd

---
+++ l1.py
@@ -0,0 +1,11 @@
+list1 = [1, 3, 4, 5]
+print(list1[0])
+
+list2 = [1, "saksham", [1, 3, 4, 5,6], True]
+
+print(list2)
+# python allows multiple data types
+# but it's a problem when massive amounts of data is used in ML
+# doing it like this is computationally slow [ ex list with terabytes of records]
+
+# instead of using list in ML we use a numPY array
---
+++ l2.py
@@ -0,0 +1,47 @@
+import numpy as np
+#numpy stands for numeric python
+#anything in a numpy array have to be of the same data type
+
+# datatype is nd array [n dimensional array]
+
+np1 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(np1)
+
+#basically arrays but more powerful
+
+var = np1.shape # bascially number of elements in our array
+print(var)
+
+np2 = np.arange(10) # 10 items in it from [0, 10)
+print(np2)
+
+np3 = np.arange(0, 10, 2)
+print(np3)
+
+
+
+
+#zeros
+
+np4 = np.zeros(10)
+print(np4)
+
+
+#multidimensional zeros
+
+np5 = np.zeros((2, 10))
+print(np5)
+
+#full 
+
+np6 = np.full((10), 6)
+print(np6)
+
+np7 = np.full((2, 10), 6)
+print(np7)
+
+#convert python list to numpy
+
+my_list = [1, 2, 3, 5, 6]
+np8 = np.array(my_list)
+print(np8)
